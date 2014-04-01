0b4c43afb4f64e6eb42edf739a7527e49b2e763e
Jason Forcier <jasonwforcier@gmail.com>
2014-04-01 12:41:11 -0700

    modified environment production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,13 @@ Realestateapp::Application.configure do
    # Required for Heroku
   # Note to set to actual host
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
